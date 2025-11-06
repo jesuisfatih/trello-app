7d10f091932b1485878aa6ac12186160e53b9728
Ad Soyad <mhmmdadgzl@outlook.com>
2025-11-06 20:00:37 +0300

    Fix menuUtils and add primaryColorConfig

---
+++ src/configs/primaryColorConfig.js
@@ -0,0 +1,11 @@
+// ShopiTrello Primary Color Config
+const primaryColorConfig = [
+  {
+    name: 'primary-1',
+    light: '#3B82F6', // Blue
+    dark: '#60A5FA'
+  }
+]
+
+export default primaryColorConfig
+
