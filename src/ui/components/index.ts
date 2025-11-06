35bc375cae7d0914b407541cc685a2d4fe363780
Ad Soyad <mhmmdadgzl@outlook.com>
2025-11-06 20:31:41 +0300

    Remove Card export

--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -7,5 +7,4 @@ export { Pagination } from './Pagination';
 export { SearchBar } from './SearchBar';
 export { FormField, TextInput, TextArea, Select } from './FormField';
 export { EmptyState } from './EmptyState';
-export { Card, CardGrid } from './Card';
 
